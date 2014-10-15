a3f0eef1264ddfae821ec268018af1b6a94103ea
rodrigoquaresma <rodrigoquaresma@gmail.com>
2014-10-15 17:12:56 -0300

    new big bang

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RdsPrototype::Application.config.secret_token = '192809700361fca18f56b18b4f5df67a57596fc0bda82350ab33c1b019307f658dd3a7263ecb4400a26bd4f11ad90aadfbb63298265bd9c5eb74184cf36d8bd1'
