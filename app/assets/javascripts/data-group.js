0500e20b2c41d395f0465564b3e81f01a5dda2d0
rodrigoquaresma <rodrigoquaresma@gmail.com>
2014-12-03 17:46:44 -0200

    put view at partial

---
+++ app/assets/javascripts/data-group.js
@@ -0,0 +1,20 @@
+//= require jquery.ui.datepicker
+//= require jquery.ui.datepicker-pt-BR
+//= require jquery.ui.autocomplete
+
+//= require jquery.tmpl.min
+//= require jquery.validate.js
+//= require jquery.action_menu
+//= require jquery.validate
+
+//= require data-tables/jquery.dataTables
+//= require data-tables/dataTables
+
+(function($){
+
+  $(document).ready(function(){
+    $(".table-actions-link").actionMenu();
+  });
+
+
+})(jQuery);
